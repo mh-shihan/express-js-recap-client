3e66b5deab8d3b057b036a69d3c610fae725678c
mh-shihan <mahmodul.no.1@gmail.com>
2025-03-14 14:23:42 +0600

    Chagne nav item

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,7 @@ import MainLayout from "./layouts/MainLayout.jsx";
 import Home from "./pages/home/Home.jsx";
 import InsertedPeoples from "./pages/inserted-peoples/InsertedPeoples.jsx";
 import Update from "./pages/update/Update.jsx";
+import Signup from "./pages/signup/Signup.jsx";
 
 const router = createBrowserRouter([
   {
@@ -25,6 +26,10 @@ const router = createBrowserRouter([
         element: <Update></Update>,
         loader: ({ params }) =>
           fetch(`http://localhost:5000/inserted-peoples/${params.id}`),
+      },
+      {
+        path: "signup",
+        element: <Signup></Signup>,
       },
     ],
   },
--- src/pages/home/Navbar.jsx
+++ src/pages/home/Navbar.jsx
@@ -1,7 +1,19 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
 const Navbar = () => {
+  const navItem = (
+    <>
+      <li>
+        <NavLink to="/">Home</NavLink>
+      </li>
+      <li>
+        <NavLink to="/input-form">Input Form</NavLink>
+      </li>
+      <li>
+        <NavLink to="/inserted-peoples">Inserted People</NavLink>
+      </li>
+    </>
+  );
   return (
     <div className="navbar bg-base-100">
       <div className="navbar-start">
@@ -26,38 +38,14 @@ const Navbar = () => {
             tabIndex={0}
             className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
           >
-            <li>
-              <a>Item 1</a>
-            </li>
-            <li>
-              <a>Parent</a>
-              <ul className="p-2">
-                <li>
-                  <a>Submenu 1</a>
-                </li>
-                <li>
-                  <a>Submenu 2</a>
-                </li>
-              </ul>
-            </li>
-            <li>
-              <a>Item 3</a>
-            </li>
+            {navItem}
           </ul>
         </div>
-        <a className="btn btn-ghost text-xl">daisyUI</a>
+        <a className="btn btn-ghost text-xl">Express Recap</a>
       </div>
       <div className="navbar-center hidden lg:flex">
         <ul className="menu menu-horizontal px-1 font-semibold text-lg">
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/input-form">Input Form</Link>
-          </li>
-          <li>
-            <Link to="/inserted-peoples">Inserted People</Link>
-          </li>
+          {navItem}
         </ul>
       </div>
       <div className="navbar-end">
